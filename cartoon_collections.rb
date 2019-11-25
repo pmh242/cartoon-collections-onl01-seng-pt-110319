153e2d99768cd21402bff41aa5aed66050fe3c02
Pedro Huertas <pmh242@gmail.com>
2019-11-25 08:20:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,8 +16,8 @@ def summon_captain_planet(array)
   nu_array
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls
+  
 end
 
 def find_the_cheese# code an argument here
