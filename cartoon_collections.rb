8f8c93938bbb3c0d38d24a9ee4fe6945fccb4443
Pedro Huertas <pmh242@gmail.com>
2019-11-25 08:14:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,6 +20,16 @@ def summon_captain_planet(array)
   end
 end
 
+def summon_captain_planet(array)
+  nu_array = []
+  i = 0 
+  while i < array.length 
+    nu_array << array[i].capitalize + "!"
+    i += 1 
+  end 
+  nu_array
+end
+
 def long_planeteer_calls# code an argument here
   # Your code here
 end
