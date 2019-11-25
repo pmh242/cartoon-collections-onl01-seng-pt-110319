026159333e78dffae520b951e07bdd0e8b52267b
Pedro Huertas <pmh242@gmail.com>
2019-11-25 05:07:57 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,11 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves = []
+  dwarves.each_with_index do |dwarf, i|
+    puts "#{i} #{dwarf}"
+    
+  end
+  
+  
 end
 
 def summon_captain_planet# code an argument here
