531e7248a974cb75664842381342fee0af0a8578
Pedro Huertas <pmh242@gmail.com>
2019-11-25 08:44:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,6 +25,9 @@ def long_planeteer_calls(array)
   end
 end
 
-def find_the_cheese
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+    array.find do |type|
+    cheese_types.include?(type)
+  end 
 end
